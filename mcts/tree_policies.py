94f13ff84d3b2259b868fac0a33f4709beadb05e
Johannes Kulick <kulick@hildensia.de>
2015-03-24 17:32:26 +0100

    remove non-class UCB1

--- mcts/tree_policies.py
+++ mcts/tree_policies.py
@@ -7,24 +7,10 @@ class UCB1(object):
         self.c = c
 
     def __call__(self, action_node):
-        if self.c == 0:  # assert that no nan values are returned for action_node.n = 0
+        if self.c == 0:  # assert that no nan values are returned
+                        # for action_node.n = 0
             return action_node.q
 
         return (action_node.q +
                 self.c * np.sqrt(2 * np.log(action_node.parent.n) /
                                  action_node.n))
-
-
-
-def ucb1(action_node, parent, c):
-    """
-    The Upper Confidence Bounds computation
-    :param action_node:
-    :param parent:
-    :param c:
-    :return:
-    """
-    if c == 0:  # assert that no nan values are returned for action_node.n = 0
-        return action_node.q
-
-    return action_node.q + c * np.sqrt(2 * np.log(parent.n) / action_node.n)
