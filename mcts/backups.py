61aba90790dd38606113ffa3b21f022b904711e3
Johannes Kulick <kulick@hildensia.de>
2015-03-24 17:10:55 +0100

    remove non-class bellman backup

--- mcts/backups.py
+++ mcts/backups.py
@@ -16,15 +16,3 @@ class Bellman(object):
                 node.q = sum([(self.gamma * x.q + x.reward) * x.n
                               for x in node.children.values()]) / n
             node = node.parent
-
-
-def bellman_backup(node, gamma):
-    while node is not None:
-        node.n += 1
-        if isinstance(node, StateNode):
-            node.q = max([x.q for x in node.children.values()])
-        elif isinstance(node, ActionNode):
-            n = sum([x.n for x in node.children.values()])
-            node.q = sum([(gamma * x.q + x.reward) * x.n
-                          for x in node.children.values()]) / n
-        node = node.parent
